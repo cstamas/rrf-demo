b5208d7e5f9874dfcc065fcb0a599b7daab64822
Tamas Cservenak <tamas@cservenak.net>
2022-10-18 15:34:44 +0200

    Fix script

--- run-it.sh
+++ run-it.sh
@@ -1,1 +1,1 @@
-~/tmp/apache-maven-3.9.0-SNAPSHOT/bin/mvn -s settings.xml -Dmaven.repo.local=local clean package -Daether.remoteRepositoryFilter.prefix=true  -Daether.remoteRepositoryFilter.groupId=true
+~/tmp/apache-maven-3.9.0-SNAPSHOT/bin/mvn -s settings.xml -Dmaven.repo.local=local clean package -Daether.remoteRepositoryFilter.prefixes=true  -Daether.remoteRepositoryFilter.groupId=true
